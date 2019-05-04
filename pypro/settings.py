6cf32fe0135cd6ccf0fdc5614bbedfe5d8c8f27c
gelhen <gelhen@gmail.com>
2019-05-04 15:12:00 -0300

    Corrigida falha de flake8

--- pypro/settings.py
+++ pypro/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
-COLLECTFAST_ENABLED=False
+COLLECTFAST_ENABLED = False
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
