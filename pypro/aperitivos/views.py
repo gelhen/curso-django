05e974e6232eee71a8c3699245eb5c3b00deeec4
andre <gelhen@gmail.com>
2021-06-07 23:41:08 -0300

    Filtros
    close #444

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(requests, slug):
-    video = dict(titulo='Video Aperitivo: Motivação', vimeo_id=337049273)
+    videos = {
+        'motivacao': dict(titulo='Video Aperitivo: Motivação', vimeo_id=337049273),
+        'instalacao-windows': dict(titulo='Video Aperitivo: Instalação Windows', vimeo_id=337049273),
+    }
+    video = videos[slug]
     return render(requests, 'aperitivos/video.html', context=dict(video=video))
