7964ad060769ed07d72029dc87d90691c985458d
andre <gelhen@gmail.com>
2021-06-10 22:19:16 -0300

    Criado lista de videos aperitivos com lista de elementos for

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
+videos = [
+    dict(slug='motivacao', titulo='Video Aperitivo: Motivação', vimeo_id=337049273),
+    dict(slug='instalacao-windows', titulo='Video Aperitivo: Instalação Windows', vimeo_id=337049273),
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+def indice(requests):
+    return render(requests, 'aperitivos/indice.html', context={'videos': videos})
 
 def video(requests, slug):
-    videos = {
-        'motivacao': dict(titulo='Video Aperitivo: Motivação', vimeo_id=337049273),
-        'instalacao-windows': dict(titulo='Video Aperitivo: Instalação Windows', vimeo_id=337049273),
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(requests, 'aperitivos/video.html', context=dict(video=video))
