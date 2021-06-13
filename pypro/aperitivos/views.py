7862bf54aeeeb11bfca0f78dd89a146acdc662cc
andre <gelhen@gmail.com>
2021-06-13 06:46:01 -0300

    Acessos a atributos de um objeto no template

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.vimeo_id = vimeo_id
+
+
 videos = [
-    dict(slug='motivacao', titulo='Video Aperitivo: Motivação', vimeo_id=337049273),
-    dict(slug='instalacao-windows', titulo='Video Aperitivo: Instalação Windows', vimeo_id=337049273),
+    Video('motivacao', 'Video Aperitivo: Motivação', 337049273),
+    Video('instalacao-windows', 'Video Aperitivo: Instalação Windows', 337049273),
 ]
 
-videos_dct = {dct['slug']: dct for dct in videos}
+videos_dct = {v.slug: v for v in videos}
 
 def indice(requests):
     return render(requests, 'aperitivos/indice.html', context={'videos': videos})
